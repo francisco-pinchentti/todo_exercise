993140bc8754f8e0efbc96b9d638422e5ae47a07
Francisco Pinchentti <francisco.pinchentti@gmail.com>
2017-12-04 14:04:45 -0300

    added a simple loading style on items dashboard

--- ui/src/app/todolist/todolist.component.ts
+++ ui/src/app/todolist/todolist.component.ts
@@ -15,6 +15,7 @@ export class TodolistComponent implements OnInit {
 
   todoItems: TodoItem[];
   username: string;
+  isRequestPending = false;
 
   constructor(
     private todoService: TodoService,
@@ -22,11 +23,13 @@ export class TodolistComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.isRequestPending = true;
     this.todoService
       .list()
       .subscribe((response: any[]) => {
-        this.todoItems = response.map( itemData => new TodoItem(itemData) );
-      });
+        this.isRequestPending = false;
+        this.todoItems = response.map(itemData => new TodoItem(itemData));
+      }, () => this.isRequestPending = false);
   }
 
   onEditClick(todoItem: TodoItem): void {
@@ -35,13 +38,15 @@ export class TodolistComponent implements OnInit {
     modalRef.componentInstance.todoItem = todoItem;
 
     modalRef.result.then((result: any) => {
+      this.isRequestPending = true;
       const updatedItem = new TodoItem(result);
       this.todoService
         .update(updatedItem)
         .subscribe((response: any) => {
+          this.isRequestPending = false;
           // update with BE data:
           todoItem.assign(response);
-        });
+        }, () => this.isRequestPending = false);
     }, (reason) => {
       console.log(`Dismissed ${this.getDismissReason(reason)}`);
     });
@@ -59,22 +64,26 @@ export class TodolistComponent implements OnInit {
 
   onDeleteClick(todoItem: TodoItem): void {
     const targetId = todoItem.id;
+    this.isRequestPending = true;
     this.todoService
-    .delete(targetId)
-    .subscribe( (response: any) => {
-      this.todoItems = this.todoItems.filter( (item: TodoItem) => item.id !== targetId);
-    });
+      .delete(targetId)
+      .subscribe((response: any) => {
+        this.isRequestPending = false;
+        this.todoItems = this.todoItems.filter((item: TodoItem) => item.id !== targetId);
+      }, () => this.isRequestPending = false);
   }
 
   onCreateClick(): void {
     const modalRef = this.modalService.open(TodoModalComponent);
     modalRef.result.then((result: any) => {
+      this.isRequestPending = true;
       const newItem = new TodoItem(result);
       this.todoService
         .create(newItem)
         .subscribe((response: any) => {
+          this.isRequestPending = false;
           this.todoItems.push(new TodoItem(response));
-        });
+        }, () => this.isRequestPending = false);
     }, (reason) => {
       console.log(`Dismissed ${this.getDismissReason(reason)}`);
     });
